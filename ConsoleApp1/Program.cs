c96df6c52f8ce84e47122dc293a7723c80332c83
Ziya Suzen <ziya@suzen.net>
2024-05-10 19:37:28 +0100

    fix warnings

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,6 +17,7 @@ foreach (var file in files)
                         #pragma warning disable CS1572
                         #pragma warning disable CS1573
                         #pragma warning disable CS8603
+                        #pragma warning disable CS8604
                         #pragma warning disable CS8618
                         #pragma warning disable CS8625
                         #pragma warning disable SA1001
@@ -28,12 +29,15 @@ foreach (var file in files)
                         #pragma warning disable SA1011
                         #pragma warning disable SA1012
                         #pragma warning disable SA1021
+                        #pragma warning disable SA1025
                         #pragma warning disable SA1027
                         #pragma warning disable SA1106
+                        #pragma warning disable SA1107
                         #pragma warning disable SA1111
                         #pragma warning disable SA1117
                         #pragma warning disable SA1119
                         #pragma warning disable SA1122
+                        #pragma warning disable SA1132
                         #pragma warning disable SA1137
                         #pragma warning disable SA1201
                         #pragma warning disable SA1202
@@ -42,26 +46,23 @@ foreach (var file in files)
                         #pragma warning disable SA1300
                         #pragma warning disable SA1303
                         #pragma warning disable SA1307
+                        #pragma warning disable SA1312
+                        #pragma warning disable SA1313
                         #pragma warning disable SA1400
+                        #pragma warning disable SA1401
                         #pragma warning disable SA1407
                         #pragma warning disable SA1413
                         #pragma warning disable SA1500
+                        #pragma warning disable SA1501
                         #pragma warning disable SA1505
+                        #pragma warning disable SA1507
                         #pragma warning disable SA1508
                         #pragma warning disable SA1512
                         #pragma warning disable SA1513
                         #pragma warning disable SA1514
                         #pragma warning disable SA1515
-                        #pragma warning disable SX1309
-                        #pragma warning disable SA1507
-                        #pragma warning disable SA1401
-                        #pragma warning disable SA1132
-                        #pragma warning disable SA1312
                         #pragma warning disable SA1520
-                        #pragma warning disable SA1107
-                        #pragma warning disable SA1313
-                        #pragma warning disable SA1501
-                        #pragma warning disable SA1025
+                        #pragma warning disable SX1309
 
                         """);
     var firstNonEmptyLineFound = false;
